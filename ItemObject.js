e1d9a331806fad9c7d65dd0dc90f324ebaddb136
Dominik H <dcdominoes@gmail.com>
2022-09-21 00:25:25 +0100

    Code cleanup

---
+++ ItemObject.js
@@ -0,0 +1,13 @@
+/**
+ * An ItemObject that holds x and y coordinates, as well as the type of object at those coordinates.
+ * @param {string} x Position x on the grid.
+ * @param {string} y Position y on the grid.
+ * @param {string} type Type of object to place on the grid.
+ */
+class ItemObject {
+    constructor(x, y, type) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+    }
+}
---
+++ Point.js
@@ -0,0 +1,11 @@
+/**
+ * A point that represents a location from (x, y) coordinates.
+ * @param {string} x
+ * @param {string} y
+ */
+ class Point {
+  constructor(x, y) {
+      this.x = x;
+      this.y = y;
+  }
+}
